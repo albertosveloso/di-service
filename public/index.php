f0e8b3c7e05443260c3a23a4afdbf927b696c580
Alberto Veloso <albertosveloso@gmail.com>
2016-02-29 21:46:03 -0300

    Lazy e Shared Services (Pimple)

--- public/index.php
+++ public/index.php
@@ -4,8 +4,24 @@
 require_once "../vendor/autoload.php";
 
 //Servico sendo chamado e não é necessario saber se cliente precisa de conexao e outros....
-$cliente = Container::getCliente();
+//$cliente = Container::getCliente();
 
-$listaCliente = $cliente->listar();
+//Trabalhando com pimple
 
-require_once "clientes.list.php";
+$container = new Pimple;
+
+//Registrando novo servico conexao compartilhado (Recurso pimple)
+//$container->share irá compartilhar a conexao e nao ira solicitar nova conexao ao criar varios objetos de clientes
+$container['conexao'] = $container->share(function(){
+    return new \APP\Conexao("localhost", "diservice", "root", "goleta");
+});
+
+$cliente = new \APP\Cliente($container['conexao']);
+
+//Usa a mesma conexao anterior, também conhecido como Lazy Services
+$cliente2 = new \APP\Cliente($container['conexao']);
+
+
+
+
+
